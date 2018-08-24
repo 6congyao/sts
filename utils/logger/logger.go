913e10c2c5de5baed11f37572a3a718b9d6db27b
=tupeng <tupengwork@qq.com>
2018-08-24 10:13:31 +0800

    UPDATE logger: use default time zone

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -90,5 +90,5 @@ type logWriter struct {
 }
 
 func (writer logWriter) Write(bytes []byte) (int, error) {
-	return fmt.Print(time.Now().UTC().Format("2006-01-02 15:04:05 ") + string(bytes))
+	return fmt.Print(time.Now().Format("2006-01-02 15:04:05 ") + string(bytes))
 }
